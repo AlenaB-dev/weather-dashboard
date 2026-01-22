7808aa35617e4852d6cc6d7dab114c6784233a86
Alena Petrych <abelokonskaya.87@gmail.com>
2026-01-22 13:11:03 +0000

    feat: add change city button component

--- weather-dashboard/src/App.jsx
+++ weather-dashboard/src/App.jsx
@@ -2,8 +2,16 @@ import "./App.css";
 import LocationHeader from "./components/LocationHeader";
 import WeatherOverview from "./components/WeatherOverview";
 import TemperatureNow from "./components/TemperatureNow";
+import ForecastList from "./components/ForecastList";
+import ChangeCityButton from "./components/ChangeCityButton";
 
 function App() {
+  const forecastMock = [
+    { day: "Tue", temp: 18, icon: "/sun.png" },
+    { day: "Wend", temp: 16, icon: "/cloud.png" },
+    { day: "Thur", temp: 19, icon: "/sun.png" },
+  ];
+
   return (
     <>
       <LocationHeader city="London" date="Monday, 22 January" />
@@ -14,6 +22,8 @@ function App() {
         description="light rain"
       />
       <TemperatureNow temperature={8.2} description="Light rain" />
+      <ForecastList forecast={forecastMock} />
+      <ChangeCityButton onclick={() => alert("Change city")} />
     </>
   );
 }
--- weather-dashboard/src/components/ChangeCityButton.jsx
+++ weather-dashboard/src/components/ChangeCityButton.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-
-function ChangeCityButton() {
-  return <div>ChangeCityButton</div>;
+function ChangeCityButton({ onclick }) {
+  return <button onclick={onclick}>Pick another city</button>;
 }
 
 export default ChangeCityButton;
