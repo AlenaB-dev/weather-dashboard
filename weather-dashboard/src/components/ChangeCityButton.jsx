fce48c42cc23f245e75e5bcf07d1e6768152b9e0
Alena Petrych <abelokonskaya.87@gmail.com>
2026-02-25 11:18:12 +0000

    refactor: delete unneeded change city button file

--- weather-dashboard/src/components/ChangeCityButton.jsx
+++
@@ -1,7 +0,0 @@
-// choose another city to see forecast
-
-function ChangeCityButton({ onclick }) {
-  return <button onClick={onclick}>Pick another city</button>;
-}
-
-export default ChangeCityButton;
