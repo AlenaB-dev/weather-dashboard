4156c20ad8b23907d0a156a894ea5fb321b79550
Alena Petrych <abelokonskaya.87@gmail.com>
2026-02-25 12:37:45 +0000

    style: add style to state and country field in heading

--- weather-dashboard/src/components/LocationHeader.jsx
+++ weather-dashboard/src/components/LocationHeader.jsx
@@ -2,9 +2,14 @@ import headerStyle from "./LocationHeader.module.css";
 import styles from "./Glass.module.css";
 
 function LocationHeader({ city, date }) {
+  const [mainCity, subCity] = city.split(/,(.+)/);
+
   return (
     <header className={`${headerStyle.locationHeader} ${styles.glass}`}>
-      <h1 className={headerStyle.city}>{city}</h1>
+      <h1 className={headerStyle.city}>
+        {mainCity}
+        {subCity && <span className={headerStyle.citySub}>{subCity}</span>}
+      </h1>
       <p className={headerStyle.date}>{date}</p>
     </header>
   );
